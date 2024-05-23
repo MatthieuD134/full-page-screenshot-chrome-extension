1f3dc403d19bd4207556e51844f5a0762c17ffbf
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2024-05-23 16:02:06 +0800

    add stylelint config

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -5,7 +5,9 @@ module.exports = {
     "stylelint-config-prettier-scss",
     "stylelint-config-tailwindcss",
   ],
+  plugins: ["stylelint-prettier"],
   rules: {
+    "prettier/prettier": true,
     "selector-class-pattern": null,
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": [
