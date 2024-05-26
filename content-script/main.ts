52c41c5672a0f5d30506c98ad339dbd64b75f82b
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2024-05-26 19:17:36 +0800

    fix images generated in canvas

--- content-script/main.ts
+++ content-script/main.ts
@@ -5,7 +5,7 @@ import { MESSAGE_ACTIONS } from "../shared/constants/chrome-message";
 // listen to message
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message?.action === MESSAGE_ACTIONS.DOWNLOAD_PDF) {
-    html2canvas(document.body).then((canvas) => {
+    html2canvas(document.body, { useCORS: true }).then((canvas) => {
       const dataUrl = canvas.toDataURL();
       sendResponse({ data: dataUrl });
     });
