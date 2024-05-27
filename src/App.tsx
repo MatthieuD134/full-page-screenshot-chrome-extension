bb2c78e91fb3af18768942111f916b2ee0c93fe4
Matthieu Daulhiac <96766017+MatthieuD134@users.noreply.github.com>
2024-05-27 11:11:34 +0800

    update logo and text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from "@src/assets/react.svg";
-import viteLogo from "@src/assets/vite.svg";
 import { Button } from "@src/component/ui/button";
 import { LoaderCircle } from "lucide-react";
 import { useState } from "react";
@@ -44,24 +42,14 @@ function App() {
   return (
     <main className="m-4 flex min-w-80 flex-col items-center gap-4">
       <div className="flex justify-center gap-10">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="w-20" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="w-20" alt="React logo" />
-        </a>
+        <img src="/images/512x512.png" alt="Save as PNG logo" className="h-20 w-20" />
       </div>
-      <h1>Vite + React</h1>
       <div className="mx-auto flex flex-col items-center gap-4">
         <Button disabled={isDownloading} onClick={downloadPNG}>
           Download PNG
           {isDownloading && <LoaderCircle className="ml-2 animate-spin" />}
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </main>
   );
 }
